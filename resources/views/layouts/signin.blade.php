771070a11679615419ff442e54a9519f672cfc55
Mugi Rachmat <infomugi@gmail.com>
2021-11-05 10:59:48 +0700

    resources/views/layouts/signin.blade: create

---
+++ resources/views/layouts/signin.blade.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Good – Bootstrap 5 HTML Admin Dashboard Template by Keenthemes</title>
+<meta charset="utf-8" />
+<meta name="description" content="Good admin dashboard live demo. Check out all the features of the admin panel. A large number of settings, additional services and widgets." />
+<meta name="keywords" content="Good, bootstrap, bootstrap 5, admin themes, free admin themes, bootstrap admin, bootstrap dashboard, bootstrap dark mode" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="article" />
+<meta property="og:title" content="Good - Bootstrap 5 HTML Admin Dashboard Template" />
+<meta property="og:url" content="https://themes.getbootstrap.com/product/good-bootstrap-5-admin-dashboard-template" />
+<meta property="og:site_name" content="Keenthemes | Good" />
+<link rel="canonical" href="https://preview.keenthemes.com/good" />
+<link rel="shortcut icon" href="{{ asset('/public/backend/assets/media/logos/favicon.ico') }}" />
+<!--begin::Fonts-->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+<!--end::Fonts-->
+<!--begin::Global Stylesheets Bundle(used by all pages)-->
+<link href="{{ asset('/public/backend/assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('/public/backend/assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+<!--end::Global Stylesheets Bundle-->
+
+</head>
+<!--end::Head-->
+<!--begin::Body-->
+<body id="kt_body" class="auth-bg">
+
+
+
+<script>var hostUrl = "{{ asset('/public/backend/assets/index.html') }}";</script>
+<!--begin::Javascript-->
+<!--begin::Global Javascript Bundle(used by all pages)-->
+<script src="{{ asset('/public/backend/assets/plugins/global/plugins.bundle.js') }}"></script>
+<script src="{{ asset('/public/backend/assets/js/scripts.bundle.js') }}"></script>
+<!--end::Global Javascript Bundle-->
+<!--begin::Page Custom Javascript(used by this page)-->
+<script src="{{ asset('/public/backend/assets/js/custom/authentication/sign-in/general.js') }}"></script>
+<!--end::Page Custom Javascript-->
+<!--end::Javascript-->
+
+</body>
+<!--end::Body-->
+</html>
